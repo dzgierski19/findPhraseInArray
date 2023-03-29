9e8754ae59a3e6cb110761648d1b8cbbde93e39f
dzgierski19 <dzgierskigit@gmail.com>
2023-03-30 00:24:10 +0200

    added .ts file

---
+++ findPhraseInArray.ts
@@ -0,0 +1,9 @@
+const inputData = ["stwórz", "sobie", "tutaj", "więcej", "wyrazów", "TUTAJ"];
+
+const findPhraseInArray = (array, phrase) => {
+  // return
+};
+
+// przykładowe działanie:
+const result = findPhraseInArray(inputData, "tut");
+// result === [ [2, 'tutaj'], [5, 'TUTAJ'] ]
