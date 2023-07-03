934522fa8e956eb926be48fa2919e1254aea7c28
dzgierski19 <dzgierskigit@gmail.com>
2023-07-04 00:55:11 +0200

    testing case

--- src/test/findPhraseInArray.test.ts
+++ src/test/findPhraseInArray.test.ts
@@ -30,6 +30,18 @@ describe("findPhraseInArray testing", () => {
       const lastElementOfResult = result[result.length - 1];
       expect(lastElementOfResult).toEqual(["Dogecoin", 6]);
     });
+    it("should return result when phrase is included in at least one element in array", () => {
+      const result = findPhraseInArray(inputData, "coin");
+      expect(result).toEqual([
+        ["Bitcoin", 0],
+        ["Litecoin", 2],
+        ["Dogecoin", 6],
+      ]);
+    });
+    it("should return empty array when phrase isn't included in any element", () => {
+      const result = findPhraseInArray(inputData, "Pepe");
+      expect(result).toHaveLength(0);
+    });
   });
 
   describe("it returns error when", () => {
@@ -42,3 +54,6 @@ describe("findPhraseInArray testing", () => {
     });
   });
 });
+
+//dopisać it z pełnym rozwiązaniem
+//Dopisać rozwiązanie z emptyArray
